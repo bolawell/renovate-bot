ecaceb948338dd6d2465d9fa846705502de8473e
Jonas Eana <jonas@eana.ro>
2021-06-29 17:48:19 +0200

    Remove repositories

--- config.js
+++ config.js
@@ -1,32 +1,10 @@
 module.exports = {
   platform: "github",
   gitAuthor: "Renovate Bot <renovate-bot@eana.ro>",
-  dryRun: true,
   token: process.env.RENOVATE_TOKEN,
 
   repositories: [
-    "eana/aws-infra-tf",
-    "eana/aws-sso",
-    "eana/bind-exporter",
-    "eana/bitwarden_rs-docker-compose",
-    "eana/blog.eana.ro",
-    "eana/ccc",
-    "eana/docker-proftpd",
-    "eana/docker-samba-server",
-    "eana/eana.ro",
-    "eana/helm_charts",
-    "eana/kubernetes-config",
-    "eana/kubernetes-monitoring",
-    "eana/memcached-exporter",
-    "eana/pihole-easylist",
-    "eana/renovate-bot",
-    "eana/renovate-config",
-    "eana/rotate-gitlab-project-tokens",
-    "eana/svtplay-dl",
-    "eana/sysadmin.compxtreme.ro",
-    "eana/sysadmin.compxtreme.ro",
-    "eana/tf-modules",
-    "eana/wordpress-snuffleupagus",
+    "bolawell/test-repo",
   ],
 
   requireConfig: true,
@@ -34,6 +12,6 @@ module.exports = {
 
   onboardingConfig: {
     $schema: "https://docs.renovatebot.com/renovate-schema.json",
-    extends: ["github>eana/renovate-config"],
+    extends: ["github>bolawell/renovate-config"],
   },
 };
